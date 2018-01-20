a42a12b077ee6a2ae38775b9404944a6fc058d6f
wikihong <hjpChinash@163.com>
2018-01-20 15:07:16 +0800

    正确的修改数据方式1-0120

--- mongoShell/demo03.js
+++ mongoShell/demo03.js
@@ -1,3 +1,15 @@
 var db = connect('company');
-db.workMate.update({'name': 'wikiHong'}, {'sex': 0});
+var workMate1 = {
+  'name': 'wikiHong',
+  'age': 18,
+  'sex': 1,
+  'job': '前端',
+  'skill': {
+    'skill1': 'html+css',
+    'skill2': 'javascript',
+    'skill3': 'swimming'
+  },
+  'regardTime': new Date()
+}
+db.workMate.update({'name': 'wikiHong'}, workMate1);
 print('[update]: The Date was update successfuly');
