cb4cbd73506c38aa2b2f62af56ce54632df5d226
wikihong <hjpChinash@163.com>
2018-01-20 15:18:03 +0800

    正确的修改数据方式2($set)-0120

--- mongoShell/demo03.js
+++ mongoShell/demo03.js
@@ -1,15 +1,19 @@
 var db = connect('company');
-var workMate1 = {
-  'name': 'wikiHong',
-  'age': 18,
-  'sex': 1,
-  'job': '前端',
-  'skill': {
-    'skill1': 'html+css',
-    'skill2': 'javascript',
-    'skill3': 'swimming'
-  },
-  'regardTime': new Date()
-}
-db.workMate.update({'name': 'wikiHong'}, workMate1);
+// var workMate1 = {
+//   'name': 'wikiHong',
+//   'age': 18,
+//   'sex': 1,
+//   'job': '前端',
+//   'skill': {
+//     'skill1': 'html+css',
+//     'skill2': 'javascript',
+//     'skill3': 'swimming'
+//   },
+//   'regardTime': new Date()
+// }
+// db.workMate.update({'name': 'wikiHong'}, workMate1);
+// print('[update]: The Date was update successfuly');
+
+// ===============1.$set修改器===================
+db.workMate.update({'name': 'wikiHong'}, {'$set': { 'sex': 2, age: 21}});
 print('[update]: The Date was update successfuly');
