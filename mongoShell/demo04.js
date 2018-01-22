95f87c9655d0cc06b83f2591fe89364101ebf304
wikihong <hjpChinash@163.com>
2018-01-22 10:41:58 +0800

    查看返回(findAndModify)-0122

--- mongoShell/demo04.js
+++ mongoShell/demo04.js
@@ -1,4 +1,14 @@
 // =============13. db.runCommand() ===================
-db.workMate.update({'sex': 1}, {$set: { 'money': 1000 }}, false, true)
-var resultMessage = db.runCommand({ getLastErr: 1 });
-printjson(resultMessage);
+// db.workMate.update({'sex': 1}, {$set: { 'money': 1000 }}, false, true)
+// var resultMessage = db.runCommand({ getLastErr: 1 });
+// printjson(resultMessage);
+
+// ====================14. findAndModify===============================
+var myModify = {
+  findAndModify: 'workMate',
+  query: { 'name': 'wikiHong' }, // 查找条件
+  update: { $set: {'age': 13}}, // 修改
+  new: true // 返回修改后的值， false是返回修改前的值
+};
+var resultMessage = db.runCommand(myModify);
+printjson(resultMessage) // 打印出json格式的数据
